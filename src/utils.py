c6a286b531cea0c62bc4f261ab0a51cdba2082d2
Kseniya <tkim70055@gmail.com>
2025-03-14 15:40:12 +0300

    HomeWork 11.1

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,14 @@
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('../logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
+
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+logger.setLevel(logging.DEBUG)
 
 
 def get_data(path):
@@ -9,8 +19,10 @@ def get_data(path):
         with open(path, encoding='utf-8') as f:
             data = json.load(f)
         if data:
+            logger.info("Возврат списка словарей с данными о финансовых транзакциях")
             return data
         else:
             return []
     except (FileNotFoundError, json.JSONDecodeError):
+        logger.error("Файл не найден")
         return []
