1de1a7398cff59ce8240d4816fd50d21cb73ace3
Kseniya <tkim70055@gmail.com>
2025-03-25 18:50:29 +0300

    HomeWork 13.1

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,35 @@
 import json
 import logging
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
-file_handler = logging.FileHandler('../logs/utils.log')
-file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
 
+log_dir = Path(__file__).parent.parent / "logs"
+log_dir.mkdir(exist_ok=True)
+
+# Настраиваем файловый обработчик
+log_file = log_dir / "utils.log"
+file_handler = logging.FileHandler(log_file)
+file_formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s')
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
-
 logger.setLevel(logging.DEBUG)
 
 
 def get_data(path):
     """
-        Загружает данные из JSON-файла и возвращает их в виде списка
+    Загружает данные из JSON-файла и возвращает их в виде списка
     """
     try:
         with open(path, encoding='utf-8') as f:
             data = json.load(f)
         if data:
-            logger.info("Возврат списка словарей с данными о финансовых транзакциях")
+            logger.info("Успешно загружены данные из файла")
             return data
-        else:
-            return []
-    except (FileNotFoundError, json.JSONDecodeError):
-        logger.error("Файл не найден")
+        return []
+    except FileNotFoundError:
+        logger.error(f"Файл не найден: {path}")
+        return []
+    except json.JSONDecodeError:
+        logger.error(f"Ошибка декодирования JSON в файле: {path}")
         return []
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,19 @@
 import json
 from unittest.mock import mock_open, patch
-
+import logging
 from src.utils import get_data
 
 
 def test_get_data():
-    mock_data = ('[{"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041", '
-                 '"operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}}, '
-                 '"description": "Перевод организации", "from": "Maestro 1596837868705199", '
-                 '"to": "Счет 64686473678894779589"}]')
+    logging.disable(logging.CRITICAL)
+
+    mock_data = '[{"id": 441945886, "state": "EXECUTED"}]'
     mock_file = mock_open(read_data=mock_data)
 
-    with patch('builtins.open', mock_file):
+    with patch('builtins.open', mock_file), \
+            patch('logging.FileHandler'):
         result = get_data("../data/operations.json")
 
     expected_result = json.loads(mock_data)
-
     assert result == expected_result
-    mock_file.assert_called_once()
+    mock_file.assert_called_once_with("../data/operations.json", encoding='utf-8')
